1c760a379ec60537de72adf3e86f8258a8c1b559
melothiagodf-df <melo.thiagodf@gmail.com>
2026-05-27 16:26:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Mude este número toda vez que fizer uma atualização
-const CACHE_VERSION = 'granafam-v5';
+const CACHE_VERSION = 'granafam-v6';
 const ASSETS = [
   '/granafam/',
   '/granafam/index.html',
